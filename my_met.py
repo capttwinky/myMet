037f39b057d481867fed42b2198fb50b3c871f9f
JP McGrady <capttwinky@gmail.com>
2012-03-26 09:21:50 -0700

    migration to 2.7, lxml complete, still needs HR datastore

--- my_met.py
+++ my_met.py
@@ -3,84 +3,88 @@ import blocks
 
 from google.appengine.api import users
 import webapp2
-#from google.appengine.ext.webapputil import run_wsgi_app
 from google.appengine.ext import db
-from google.appengine.ext.webapp import template
+from datetime import datetime
+import time
+import jinja2
+import os
 
-class busStop(db.Model):
-	stopId = db.StringProperty()
-	stopPosition = db.ListProperty(str)
-	stopDesc = db.StringProperty()
-	reqDate = db.DateTimeProperty(auto_now_add=True)
-	reqUser = db.UserProperty()
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class BusStop(db.Model):
+    stopId = db.StringProperty()
+    stopPosition = db.ListProperty(str)
+    stopDesc = db.StringProperty()
+    reqDate = db.DateTimeProperty(auto_now_add=True)
+    reqUser = db.UserProperty()
 
 class MainPage(webapp2.RequestHandler):
-	def get(self):		
-		rendTemp(self)
-		
+    def get(self):      
+        rendTemp(self)
+        
 class AboutPage(webapp2.RequestHandler):
-	def get(self):
-		dictout=dict(infoBlock = blocks.strInfo)
-		rendTemp(self, dictout)
-		
+    def get(self):
+        dictout=dict(infoBlock = blocks.strInfo)
+        rendTemp(self, dictout)
+        
 class ErrPage(webapp2.RequestHandler):
-	def get(self):
-		dictout = dict(errMsg = self.request.get('oops'))
-		rendTemp(self, dictout)
+    def get(self):
+        dictout = dict(errMsg = self.request.get('oops'))
+        rendTemp(self, dictout)
 
 class CheckStop(webapp2.RequestHandler):
 
-	def post(self):
-		self.stopToCheck = self.request.get('content').strip()
-		myConnection = pyMet.pyMet()  ###This is where you need to enter your trimet app-id 
-		if self.stopToCheck != "":
-			dictOut = {}
-			
-			bArrival = myConnection.showStopSMS(self.stopToCheck)						
-			if bArrival == False:   #trimet reported an error
-				strErUrl = '/oops?oops=%s'%(urllib2.quote(",".join(myConnection.lstOut)))
-				self.redirect(strErUrl)
-				return False 
+    def post(self):
+        self.stopToCheck = self.request.get('content').strip()
+        myConnection = pyMet.pyMet()  ###put your trimet app-id as the only line in a file called appkey.app 
+        if self.stopToCheck != "":
+            dictOut = {'req_time':datetime.strftime(datetime.now(),pyMet.TSTRFMT)}
+            bArrival = myConnection.showStopSMS(self.stopToCheck)                       
+            if bArrival == False:   #trimet reported an error
+                strErUrl = '/oops?oops=%s'%(urllib2.quote(",".join(myConnection.lstOut)))
+                self.redirect(strErUrl)
+            else:
+                 
 
-			dictOut['chkdStop'] = self.stopToCheck
-			dictOut['stpInfo'] = ",".join([myConnection.stopName,myConnection.stopDir])
-			if len(myConnection.lstOut) > 0:
-				dictOut['strArvls'] = '<br>'.join(myConnection.lstOut)
-			else:
-				dictOut['strArvls'] = ' No arrivals found for this stop at this time '
-			rendTemp(self,dictOut)
-			
-			recStop = busStop()
-			recStop.stopId = self.stopToCheck
-			recStop.stopPosition = [str(myConnection.stopLat), str(myConnection.stopLng)]
-			recStop.stopDesc = "%s, %s"%(myConnection.stopName,myConnection.stopDir)
-			if self.curUser:
-				recStop.reqUser = self.curUser
-			recStop.put()
-			return True	
-		else:  #no stop to check - load error
-			self.redirect('/oops?oops=Please%20enter%20a%20stop') 
-	def get(self):
-		self.post()
-		
+                dictOut['chkdStop'] = self.stopToCheck
+                dictOut['stpInfo'] = ",".join([myConnection.stopName,myConnection.stopDir])
+                if len(myConnection.lstOut) > 0:
+                    dictOut['strArvls'] = '<br>'.join(myConnection.lstOut)
+                else:
+                    dictOut['strArvls'] = ' No arrivals found for this stop at this time '
+                rendTemp(self,dictOut)
+                recStop = BusStop(key_name='{0}{1}'.format(self.stopToCheck, time.time()))
+                recStop.stopId = self.stopToCheck
+                recStop.stopPosition = [str(myConnection.stopLat), str(myConnection.stopLng)]
+                recStop.stopDesc = "%s, %s"%(myConnection.stopName,myConnection.stopDir)
+                if self.curUser:
+                    recStop.reqUser = self.curUser
+                recStop.put() 
+        else:  #no stop to check - load error
+            self.redirect('/oops?oops=Please%20enter%20a%20stop') 
+    def get(self):
+        self.post()
+        
 
 def rendTemp(self,dictOut = {}):
-	dictOut['loginBlock'] = blocks.getUserinfo(self.request.uri, self)
-	dictOut['strLicence'] = blocks.strLicence
-	if self.curUser:
-		dictOut['statTab'] = blocks.makeStopTable(self.curUser)
-	else:
-		dictOut['statTab'] = blocks.makeStopTable("")
-	temPpath = os.path.join(os.path.dirname(__file__),'myMet.html')
-	self.response.out.write(template.render(temPpath, dictOut))	
+    dictOut['loginBlock'] = blocks.getUserinfo(self.request.uri, self)
+    dictOut['strLicence'] = blocks.strLicence
+    if self.curUser:
+        dictOut['statTab'] = blocks.makeStopTable(self.curUser)
+    else:
+        dictOut['statTab'] = blocks.makeStopTable("")   
+    template = jinja_environment.get_template('myMet.html')
+    self.response.out.write(template.render(dictOut))    
 
 app = webapp2.WSGIApplication(
-									 [
-									  ('/stop', CheckStop),
-									  ('/about', AboutPage),
-									  ('/oops', ErrPage),
-									  ('/', MainPage)
-									  ],
-									 debug=True)    
+                                     [
+                                      ('/stop', CheckStop),
+                                      ('/about', AboutPage),
+                                      ('/oops', ErrPage),
+                                      ('/', MainPage)
+                                      ],
+                                     debug=True)    
 
-	
+    
--- pyMet.py
+++ pyMet.py
@@ -3,7 +3,6 @@ Created on Jul 10, 2010
 
 @author: JP McGrady
 
-
 --setup connection to triMet data service with appId
 --return xml parsed into elementTree from function call
 ----construct request URL
@@ -11,111 +10,111 @@ Created on Jul 10, 2010
 ----parse XML
 --print out elementTree
 '''
-import xml.etree.ElementTree as ET
-import urllib2, cStringIO, time, datetime
+from lxml import etree
+import urllib2
+import time
+import datetime
+import os
+
+TSTRFMT = '%d%b%H%M'
 
 class pyMet(object):
-  def __init__(self,appId = ""):
-	self.appId = appId
-	self.lstOut = []
-	self.stopName = []
-	self.stopDir = []
-	if self.connect() == False:
-	  raise Exception('no connection')
+    def __init__(self,appId = None):
+        if not appId:
+            try:
+                with open('{0}/{1}'.format(os.path.dirname(__file__),'appkey.app')) as ofile:
+                    appId = ofile.read().strip('\n')
+            except Exception as e:
+                print str(e)
+                raise Exception('must have an appkey.app')
+        self.appId = appId
+        self.lstOut = []
+        self.stopName = []
+        self.stopDir = []
+        if self.connect() == False:
+          raise Exception('no connection')
 
-  def connect(self):
-	if self.appId != "":			#this is a placeholder for "real" appId validation
-	  #print("appId is: %s" %self.appId)
-	  return None
-	raise Exception("no appId")
-	return False
+    def connect(self):
+        if self.appId != "":            #this is a placeholder for "real" appId validation
+          return None
+        raise Exception("no appId")
 
-  def getArrivals(self,stopId=""):
-	if stopId =="":
-	  raise Exception('no stopId')
-	strUrl = "http://developer.trimet.org/ws/V1/arrivals/locIDs/%s/appID/%s"%(stopId,self.appId)
-	fileXml = urlToFile(strUrl)
-	metTree = ET.ElementTree().parse(fileXml)
-	fileXml.close()
-	return metTree
-	
-  def getTripPlan(self,strStart,strEnd):
-	  
-	  #strUtl ="http://developer.trimet.org/ws/V1/trips/tripplanner/fromplace/pdx/toplace/rose%20quarter/appId/D93C6F933BC90C056D4EE8406"
-	  strUtl ="http://developer.trimet.org/ws/V1/trips/tripplanner/fromplace/%s/toplace/%s/appId/%s"%(strStart,strEnd,self.appId)
-	  pass
+    def getArrivals(self,stopId=""):
+        if stopId =="":
+          raise Exception('no stopId')
+        strUrl = "http://developer.trimet.org/ws/V1/arrivals/locIDs/%s/appID/%s"%(stopId,self.appId)
+        return etree.fromstring(urlToStr(strUrl).replace('xmlns="urn:trimet:arrivals"',''))
 
-  def parseArrivalsShort(self, metTree = ""):
-	lstOut=[]
-	if metTree == "":
-	  raise Exception("No Arrivals")
-	errTree = metTree.getiterator('{urn:trimet:arrivals}errorMessage')
-	locTree = metTree.getiterator('{urn:trimet:arrivals}location')
-	arrivalTree = metTree.getiterator('{urn:trimet:arrivals}arrival')
-	if len(errTree) != 0:
-		for thing in errTree:
-			self.lstOut.append(thing.text)
-		return False
-	dictLoc = locTree[0].attrib
-	self.stopName = dictLoc['desc']
-	self.stopDir = dictLoc['dir']
-	self.stopLat = dictLoc['lat']
-	self.stopLng = dictLoc['lng']
-	for lstArrivalinfo in arrivalTree:
-	  dictArrival = lstArrivalinfo.attrib
-	  if dictArrival['status'] == 'estimated':
-		strArrival = dictArrival['estimated'][:-3]+"."+dictArrival['estimated'][-3:-2]
-		fltArv = float(strArrival)
-		tdArv = datetime.timedelta(seconds=fltArv)
-		tdNow = datetime.timedelta(seconds=time.time())
-		tdWait = tdArv-tdNow
-		tmDelta = datetime.time(hour=tdWait.seconds//3600, minute=tdWait.seconds//60)
-		if tmDelta.hour > 0:
-		  strTime = tmDelta.strftime('%Hh%Mm').lstrip('0')
-		else:
-		  strTime = tmDelta.strftime('%Mm').lstrip('0')
-		  if len(strTime) == 1:
-			strTime = "now"
-	  else:
-		strArrival = dictArrival['scheduled'][:-3]+"."+dictArrival['scheduled'][-3:-2]
-		fltArv = float(strArrival) - (8* 3600) #gotta make GMT into PST
-		tmLocal = time.gmtime(fltArv)
-		#tmLocal[2] = tmLocal[2] -8
-		strTime = time.strftime('%d%b%H%M', tmLocal)
-	  strSign = reduceDoubles(dictArrival['shortSign'], ' ')
-	  lstSign = strSign.split(" ")
-	  if lstSign[1] == 'To': del(lstSign[1])
-	  strSign = " ".join(lstSign)
-	  strOut = "%s %s" %(strSign, strTime)
-	  lstOut.append(strOut)
-	self.lstOut = lstOut
-	return True
+    def getTripPlan(self,strStart,strEnd):
+      
+      #strUtl ="http://developer.trimet.org/ws/V1/trips/tripplanner/fromplace/pdx/toplace/rose%20quarter/appId/D93C6F933BC90C056D4EE8406"
+      strUtl ="http://developer.trimet.org/ws/V1/trips/tripplanner/fromplace/%s/toplace/%s/appId/%s"%(strStart,strEnd,self.appId)
+      pass
 
-  def showStopSMS(self, stopId):
-	self.arrivalTree = self.getArrivals(stopId)
-	return self.parseArrivalsShort(self.arrivalTree)
+    def parseArrivalsShort(self, metTree = ''):
+        lstOut=[]
+        if metTree == '':
+          raise Exception("No Arrivals")
+        errTree = metTree.findall('errorMessage')
+        locTree = metTree.findall('location')
+        arrivalTree = metTree.findall('arrival')
+        for thing in errTree:
+            self.lstOut.append(thing.text)
+        if self.lstOut: return False
+        dictLoc = locTree[0].attrib
+        self.stopName = dictLoc['desc']
+        self.stopDir = dictLoc['dir']
+        self.stopLat = dictLoc['lat']
+        self.stopLng = dictLoc['lng']
+        strTime = datetime.datetime.strftime(datetime.datetime.now(),TSTRFMT)
+        for arrival_info in arrivalTree:
+            if arrival_info.attrib['status'] == 'estimated':
+                strArrival = arrival_info.attrib['estimated'][:-3]+"."+arrival_info.attrib['estimated'][-3:-2]
+                fltArv = float(strArrival)
+                tdArv = datetime.timedelta(seconds=fltArv)
+                tdNow = datetime.timedelta(seconds=time.time())
+                tdWait = tdArv-tdNow
+                tmDelta = datetime.time(hour=tdWait.seconds//3600, minute=tdWait.seconds//60)
+                if tmDelta.hour > 0:
+                  strTime = tmDelta.strftime('%Hh%Mm').lstrip('0')
+                else:
+                  strTime = tmDelta.strftime('%Mm').lstrip('0') 
+                  if len(strTime) == 1:
+                    strTime = "now"
+            else:
+                strArrival = arrival_info.attrib['scheduled'][:-3]+"."+arrival_info.attrib['scheduled'][-3:-2]
+                fltArv = float(strArrival) - (8* 3600) #gotta make GMT into PST
+                tmLocal = datetime.datetime.fromtimestamp(fltArv)
+                diff = tmLocal - datetime.datetime.now()
+                if diff.days < 0:
+                    tmLocal = tmLocal+datetime.timedelta(hours=1)
+                strTime = datetime.datetime.strftime(tmLocal,TSTRFMT)
+            lstSign = reduceDoubles(arrival_info.attrib['shortSign'], ' ').split(" ")
+            if len(lstSign)>=2 and lstSign[1] == 'To': del(lstSign[1])
+            lstOut.append("%s %s" %(" ".join(lstSign), strTime))
+        self.lstOut = lstOut
+        return True
 
-def urlToFile(strUrl):
-  return cStringIO.StringIO(urllib2.urlopen(strUrl).read())
+    def showStopSMS(self, stopId):
+        self.arrivalTree = self.getArrivals(stopId)
+        return self.parseArrivalsShort(self.arrivalTree)
+
+def urlToStr(strUrl):
+  return urllib2.urlopen(strUrl).read()
 
 def shortenToSMS(lstTarget):
   strOut = ", ".join(lstTarget)
   if len(strOut) > 159:
-	strOut = shortenToSMS(lstTarget[:-1])
+    strOut = shortenToSMS(lstTarget[:-1])
   return strOut
 
 def reduceDoubles(strTarget, strGetridof):
   strOut = strTarget.replace(2*strGetridof,strGetridof)
   if strOut.find(2*strGetridof) != -1:
-	strOut = reduceDoubles(strOut, strGetridof)
+    strOut = reduceDoubles(strOut, strGetridof)
   return strOut
 
 if __name__ == '__main__':
-  myConnection = pyMet('Need app-id from tri-met here')
+  myConnection = pyMet()
   myConnection.showStopSMS('13243')
   myConnection.showStopSMS('bob')
-#  for line in myConnection.lstOut:
-#	print line
-  #strOut = shortenToSMS(myConnection.lstOut)
-  #print ((strOut), len(strOut))
-  #print("%s going %s"%(myConnection.stopName,myConnection.stopDir))
